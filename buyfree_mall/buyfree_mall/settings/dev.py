80b3fc50431bd5d1c37428cc03384e83eb0e820f
Richard Xie <defaulttest@sina.com>
2018-09-10 10:22:54 -0400

    修改allow host 配置& 增加 域名设置

--- buyfree_mall/buyfree_mall/settings/dev.py
+++ buyfree_mall/buyfree_mall/settings/dev.py
@@ -29,7 +29,7 @@ SECRET_KEY = '!@6$2#+es%24#kbbq^!_=54s@7b2o@g3wfyy(rx@=mcvf8)%gs'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['api.buyfree.site', '127.0.0.1', 'localhost', 'www.buyfree.site']
 
 # Application definition
 
