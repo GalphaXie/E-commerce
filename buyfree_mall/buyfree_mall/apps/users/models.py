8f15a05b9b1ee70ad4fec9a3197b90de5d7292c7
Richard Xie <defaulttest@sina.com>
2018-09-10 09:02:32 -0400

    添加用户模型类(继承，并增加手机号的字段)

--- buyfree_mall/buyfree_mall/apps/users/models.py
+++ buyfree_mall/buyfree_mall/apps/users/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     # 只有手机号是我们需要，而且默认没有的
-    mobile = models.CharField(max_length=11, unique=True, verbose_name="手机号")
+    mobile = models.CharField(max_length=11, unique=True, verbose_name="手机号")  # 选项unique是必须的，其次 acquire=True默认
 
     class Meta:
         db_table = "tb_users"
